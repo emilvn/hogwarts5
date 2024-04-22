914f61440097f2595c525a105af8e95300b3b46b
emilvn <emilvnielsen@hotmail.com>
2024-04-22 11:19:24 +0200

    test

---
+++ src/main/java/dk/kea/dat3js/hogwarts5/generic/PersonWithNames.java
@@ -0,0 +1,12 @@
+package dk.kea.dat3js.hogwarts5.generic;
+
+public interface PersonWithNames {
+    String getFirstName();
+    String getMiddleName();
+    String getLastName();
+    void setFirstName(String firstName);
+    void setMiddleName(String middleName);
+    void setLastName(String lastName);
+
+
+}
