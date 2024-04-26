175485bdfbc813657ab005a26464c806455899d8
emilvn <emilvnielsen@hotmail.com>
2024-04-26 10:25:18 +0200

    integration tests set up to use h2 database

--- src/test/java/dk/kea/dat3js/hogwarts5/student/StudentControllerIntegrationTest.java
+++ src/test/java/dk/kea/dat3js/hogwarts5/student/StudentControllerIntegrationTest.java
@@ -3,11 +3,14 @@ package dk.kea.dat3js.hogwarts5.student;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 public class StudentControllerIntegrationTest {
 
     @Autowired
@@ -16,5 +19,59 @@ public class StudentControllerIntegrationTest {
     @Test
     void notNull(){
         assertNotNull(webClient);
+    }
+
+    @Test
+    void createStudentWithFullName(){
+        webClient
+                .post().uri("/students")
+                .header("Content-Type", "application/json")
+                .bodyValue(
+                        """
+                        {
+                            "name": "Firstname Middlename Lastname",
+                            "isMale": true,
+                            "house": "Gryffindor",
+                            "schoolYear": 7
+                        }
+                        """
+                )
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(StudentResponseDTO.class)
+                .value(response -> {
+                    assertEquals("Firstname", response.firstName());
+                    assertEquals("Middlename", response.middleName());
+                    assertEquals("Lastname", response.lastName());
+                    assertEquals("Firstname Middlename Lastname", response.name());
+                });
+    }
+
+    @Test
+    void createStudentWithNameParts(){
+        webClient
+                .post().uri("/students")
+                .header("Content-Type", "application/json")
+                .bodyValue(
+                        """
+                        {
+                            "firstName": "Firstname",
+                            "middleName": "Middlename",
+                            "lastName": "Lastname",
+                            "isMale": true,
+                            "house": "Gryffindor",
+                            "schoolYear": 7
+                        }
+                        """
+                )
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(StudentResponseDTO.class)
+                .value(response -> {
+                    assertEquals("Firstname", response.firstName());
+                    assertEquals("Middlename", response.middleName());
+                    assertEquals("Lastname", response.lastName());
+                    assertEquals("Firstname Middlename Lastname", response.name());
+                });
     }
 }
